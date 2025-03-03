bd48d003bd4207ffc50c594efd6ed0f54c39d8a5
Abdallah-sala7 <Abdallah-Sala7@users.noreply.github.com>
2025-03-03 19:15:39 +0200

    Update Build

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,26 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // images: {
-  //   loader: "cloudinary",
-  //   path: "https://res.cloudinary.com/ugwutotheeshoes/image/upload/",
-  // },
-
+  swcMinify: true,
+  experimental: {
+    esmExternals: false,
+  },
+  distDir: "build",
   images: {
     domains: ["res.cloudinary.com"],
+  },
+  webpack: (config: {
+    watchOptions: { poll: number; aggregateTimeout: number };
+    parallelism: number;
+    infrastructureLogging: { level: string };
+  }) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    config.parallelism = 1;
+    config.infrastructureLogging = { level: "error" };
+    return config;
   },
 };
 
